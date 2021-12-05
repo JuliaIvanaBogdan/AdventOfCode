fb1f1f25730eecc0a2087754125e1d2f1de3e8c8
Julia Bogdan <95418275+JuliaIvanaBogdan@users.noreply.github.com>
2021-12-05 15:38:14 +0100

    Update day3.cpp

--- day3.cpp
+++ day3.cpp
@@ -9,7 +9,7 @@ int powerconsumption(){
     int gammarate=0;
     int epsilonrate=0;
 
-    std::ifstream input ("C:\\Users\\Julia\\Documents\\VSCode\\AdventOfCode\\day3\\day3input");
+    std::ifstream input ("input file location");
 
     char c;
     int sum[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
@@ -50,50 +50,7 @@ int powerconsumption(){
     int oxygengeneratorrating = 0;
     int CO2scrubberrating = 0;
 
-    std::ifstream input ("C:\\Users\\Julia\\Documents\\VSCode\\AdventOfCode\\day3\\day3input");
-
-    char c;
-    int values[1000][12];
-    int sum[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
-    int m = 0;
-    int n = 0;
-    while (input >> c){
-        int v = c - '0'; 
-        values[n][m] = v;
-        sum[m]+=v;
-        if (m==11){
-            m=0;
-            n+=1;
-        }
-        else{
-            m++;
-        }
-    }
-
-    for (int i=0;i<12;i++){
-        if(sum[i]>=500){
-            sum[i]=1;
-        }else{
-            sum[i]=0;
-        }
-    }
-    //1=not taken 0=taken
-
-    //would have to be done with lists
-    int findO[1000] = {0};
-    int findCO2[1000] = {0};
-
-    for (int i=0;i<12;i++){
-        for(int j=0;j<1000;j++){
-            if(sum[i]==values[j][i]){
-                findCO2[j]=1;
-            }else{
-                findO[j]=1;
-            }
-        }
-    }
-
-    
+    //TO DO
 
     return oxygengeneratorrating*CO2scrubberrating;
 }*/
