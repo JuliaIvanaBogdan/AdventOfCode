7d9db4f06f89b99021b1819940933d54fc2620a7
Julia Bogdan <95418275+JuliaIvanaBogdan@users.noreply.github.com>
2021-12-05 16:58:47 +0100

    Add files via upload

---
+++ 2021/day1.java
@@ -0,0 +1,76 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class day1 {
+	
+	
+	public static int partone() throws FileNotFoundException {
+		File input = new File("C:\\Users\\julia\\eclipse-workspace\\AdventOfCode\\Day1\\day1input");
+		Scanner sc = new Scanner(input);
+		
+		//int[] depth = new int[2000];
+		//int c = 0;
+		int increase = 0;
+		int prev = sc.nextInt();
+		int current = 0;
+		while (sc.hasNextLine()) {
+			//depth[c] = sc.nextInt();
+			//c++;
+			current = sc.nextInt();
+			if (prev<current) {
+				increase++;
+				prev = current;
+			} else {
+				prev = current;
+			}
+			
+		}
+		
+		return increase;
+	}
+	
+	
+	
+
+	public static int parttwo() throws FileNotFoundException {
+		File input = new File("C:\\Users\\julia\\eclipse-workspace\\AdventOfCode\\Day1\\day1input");
+		Scanner sc = new Scanner(input);
+		
+		int increase = 0;
+		
+		
+		int[] depth = new int[2000];
+		int c = 0;
+		
+		
+		while (sc.hasNextLine()) {
+			depth[c] = sc.nextInt();
+			c++;			
+		}
+		
+		for (int i=0;i<depth.length-3;i++) {
+			int A = depth[i]+depth[i+1]+depth[i+2];
+			int B = depth[i+1]+depth[i+2]+depth[i+3];
+			
+			if (A<B) {
+				increase++;
+			}
+		}
+		
+		return increase;
+	}
+
+	
+	
+
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		//int solution = partone();
+		int solution = parttwo();
+		System.out.println(solution);
+		return;
+	}
+	
+}
