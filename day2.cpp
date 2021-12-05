49cd1895383fe76df326701063f81b7605332d92
Julia Bogdan <95418275+JuliaIvanaBogdan@users.noreply.github.com>
2021-12-05 15:28:13 +0100

    add day2

---
+++ day2.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+
+
+int stepone(){
+    int horizontal = 0;
+    int depth = 0;
+
+    std::ifstream input;
+    input.open("C:\\Users\\Julia\\Documents\\VSCode\\AdventOfCode\\day2input");
+
+    if(!input){
+        printf("problem opening file");
+    }
+
+    char* direction = new char[10];
+    int scale;
+
+    while(input >> direction >> scale){
+
+        if (direction[0] == 'f'){
+            horizontal += scale;  
+        }
+        else if(direction[0] == 'd'){
+            depth += scale;
+        }
+        else if( direction[0] == 'u'){
+            depth -= scale;
+        }
+    }
+    return horizontal * depth;
+}
+
+
+int steptwo(){
+    int horizontal = 0;
+    int depth = 0;
+    int aim = 0;
+
+    std::ifstream input;
+    input.open("C:\\Users\\Julia\\Documents\\VSCode\\AdventOfCode\\day2input");
+
+    if(!input){
+        printf("problem opening file");
+    }
+
+    char* direction = new char[10];
+    int scale;
+
+    while(input >> direction >> scale){
+
+        if (direction[0] == 'f'){
+            horizontal += scale;
+            depth += scale * aim;  
+        }
+        else if(direction[0] == 'd'){
+            aim += scale;
+        }
+        else if( direction[0] == 'u'){
+            aim -= scale;
+        }
+    }
+    return horizontal * depth;
+}
+
+
+int main(){
+    //int solution = stepone();
+    int solution = steptwo();
+    printf("%d", solution);
+    return 0;
+}
+
